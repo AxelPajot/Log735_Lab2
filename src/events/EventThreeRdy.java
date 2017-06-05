853337fb130b239ff22a441bc5dc3431b887e29b
axelpajot <axel.pajot.1@ens.etsmtl.ca>
2017-06-05 16:53:32 -0400

    Correction erreur interface event 3

--- src/events/EventThreeRdy.java
+++ src/events/EventThreeRdy.java
@@ -1,6 +1,6 @@
 package events;
 
-public class EventThreeRdy extends EventBase implements IEventThreeRdy{
+public class EventThreeRdy extends EventBase implements IEventThreeRdy, IEventRdy{
 
 	private static final long serialVersionUID = 6784739856767178L;
 
